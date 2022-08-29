d2a8f2e18d0ace881508630be0481d94985814d9
Thomas Ingeman-Nielsen <tin@byg.dtu.dk>
2022-08-29 17:51:11 +0200

    Added reboot_now and shutdown_now scripts

---
+++ dtu-ert-pi/sh_scripts/reboot_now.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# If this script is sourced from the install script, the variable
+# DTUERTPI_DIR will be set and can be used as the base for determining
+# folder structure.
+# If not set, get the current directory from the bash environment
+if [[ -z $DTUERTPI_DIR ]]; then
+  SH_SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+else
+  SH_SCRIPTS_DIR="$DTUERTPI_DIR"/sh_scripts
+fi
+
+# import settings
+source "$SH_SCRIPTS_DIR"/script_settings
+source "$WITTYPI_DIR"/wittyPi.conf
+
+touch $LOGDIR/logfile
+$BIN/echo `date "+%Y-%m-%d %H:%M:%S(%z)"` "=== INITIATING FORCED REBOOT ==========================" >> $LOGDIR/logfile 
+sleep 2
+
+clear_alarm_flags
+shutdown -r now
+
+
+
+
