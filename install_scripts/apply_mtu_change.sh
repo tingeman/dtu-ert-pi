c4480c25c2dff18eee45fc58c819a9d329a0aa7f
Thomas Ingeman-Nielsen <tin@byg.dtu.dk>
2022-09-12 12:30:48 +0000

    Fixed apply_mtu_change to correctly handle commented line

--- install_scripts/apply_mtu_change.sh
+++ install_scripts/apply_mtu_change.sh
@@ -14,19 +14,21 @@ fi
 
 echo " "
 echo ">>> Updating /etc/ppp/options with new MTU setting..."
-match=$(grep '^[[:blank:]]*[^[:blank:]#]' /etc/ppp/options | grep '^mtu')
+
+match=$(grep '^[[:blank:]#]*mtu' /etc/ppp/options)
 match=$(echo -e "$match" | sed -e 's/^[[:space:]]*//')
 if [[ -z "$match" ]]; then
     # if line is missing, insert it 
     echo "mtu 1200" >> /etc/ppp/options
     echo "Inserted missing line"
 elif [[  "$match" == "#"* ]]; then
-    # if line is commented, insert new line after the commented line
-    sed -i 's/^[[:space:]]*#[[:space:]]*mtu .*/&\nmtu 1200/' /etc/ppp/options
-    echo "Found commented line, inserting new line after it"
+    # if line is commented, replace it
+    sed -i 's/^[[:space:]]*#[[:space:]]*mtu .*/mtu 1200/' /etc/ppp/options
+    echo "Found commented line, and replaced it"
 else
     # if line exists, replace it
-    sed -i "s/^\s*\(mtu .*\)/mtu 1200/" /etc/ppp/options
+    #sed -i "s/^\s*\(mtu .*\)/mtu 1200/" /etc/ppp/options
+    sed -i "s/^\s*mtu .*/mtu 1200/" /etc/ppp/options
     echo 'Found existing line and replaced it.'
 fi
 
