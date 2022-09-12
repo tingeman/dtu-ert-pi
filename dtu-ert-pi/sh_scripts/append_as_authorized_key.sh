7018ff0f142a27d352196914d848b19870e2cfd1
Thomas Ingeman-Nielsen <tin@byg.dtu.dk>
2022-09-12 22:33:42 +0000

    Fixed bugs in append_as_authorized_key.sh

--- dtu-ert-pi/sh_scripts/append_as_authorized_key.sh
+++ dtu-ert-pi/sh_scripts/append_as_authorized_key.sh
@@ -4,13 +4,13 @@ if [[ -z $SCRIPTS_DIR ]]; then
     SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 fi
 
-source "$SCRIPTS_DIR/cronscripter_settings"
+source "$SCRIPTS_DIR/script_settings"
 
 /usr/bin/ssh-copy-id -i "$SSHKEY".pub $SERVER
 
 if [[ $? -eq 0 ]]; then
     echo "SSH key has been added to the authorized_keys of the host."
-    echo "Test access with the command: ssh -i $SSHKEY $IOT"
+    echo "Test access with the command: ssh -i $SSHKEY $SERVER"
 else
     echo "Could not add key to hosts authorized keys!"
     echo "Please do it manually."
