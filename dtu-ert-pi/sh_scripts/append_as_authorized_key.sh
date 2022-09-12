26aea0f7b65b33dc35e6893a60ec61c29248f525
Thomas Ingeman-Nielsen <tin@byg.dtu.dk>
2022-09-12 22:26:44 +0000

    Added script to append ssh key to upstream server authorized keys

---
+++ dtu-ert-pi/sh_scripts/append_as_authorized_key.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [[ -z $SCRIPTS_DIR ]]; then
+    SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+fi
+
+source "$SCRIPTS_DIR/cronscripter_settings"
+
+/usr/bin/ssh-copy-id -i "$SSHKEY".pub $SERVER
+
+if [[ $? -eq 0 ]]; then
+    echo "SSH key has been added to the authorized_keys of the host."
+    echo "Test access with the command: ssh -i $SSHKEY $IOT"
+else
+    echo "Could not add key to hosts authorized keys!"
+    echo "Please do it manually."
+fi
+
+
