8576ba6f2c9f51101ed0bc052a908d73efff2ba6
Thomas Ingeman-Nielsen <tin@byg.dtu.dk>
2022-09-12 14:23:05 +0000

    fixed path in enable_systemd_daily_reboot.sh and added crontab template

--- install_scripts/enable_systemd_daily_reboot.sh
+++ install_scripts/enable_systemd_daily_reboot.sh
@@ -27,7 +27,7 @@ echo " "
 echo ">>> Installing forced_reboot timer to reboot at 04:00 UTC every day"
 
 echo "Copying systemd files from templates..."
-cp -f $SH_SCRIPTS_DIR/templates/forced_reboot.* /etc/systemd/system/
+cp -f $SH_SCRIPTS_DIR/template_files/forced_reboot.* /etc/systemd/system/
 
 echo "Reloading daemons..."
 systemctl daemon-reload
@@ -36,5 +36,8 @@ echo "Enabling timer..."
 systemctl enable --now forced_reboot.timer
 
 # don't enable the forced_reboot.service. That would result in the reboot running immediately at boot (infinite loop)
+
+# reconfigure crontab
+$SH_SCRIPTS_DIR/auto_configure_crontab.sh crontab_template_noreboot.txt
 
 echo "done"
