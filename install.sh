2d9752d83e80aa879d18c2ffd4a3f8c8db82846b
Thomas Ingeman-Nielsen <tin@byg.dtu.dk>
2022-09-13 08:41:05 +0000

    Updated install script with possibility to install live_test branch

--- install.sh
+++ install.sh
@@ -40,7 +40,7 @@ HOSTNAME=$(hostname)
 CRONTAB_TEMPLATE=$DTUERTPI_DIR/sh_scripts/template_files/crontab_template_shutdown.txt
 
 # [GIT BRANCH] --------------------------------------------
-GIT_BRANCH=develop      # master or develop
+GIT_BRANCH=live_test      # master, develop or live_test
 
 # [UPLOAD SERVER] -----------------------------------------
 SERVER_IP="192.38.64.71"
@@ -133,6 +133,9 @@ else
   elif [[ $GIT_BRANCH == master ]]; then
     wget https://github.com/tingeman/dtu-ert-pi/archive/refs/heads/master.zip -O "$TMP_DIR/dtu-ert-pi.zip"
     SRC_DIR="$TMP_DIR"/dtu-ert-pi-master
+  elif [[ $GIT_BRANCH == live_test ]]; then
+    wget https://github.com/tingeman/dtu-ert-pi/archive/refs/heads/live_test.zip -O "$TMP_DIR/dtu-ert-pi.zip"
+    SRC_DIR="$TMP_DIR"/dtu-ert-pi-live_test
   else
     echo 'Unknown git branch specified, aborting!'
     exit 1
